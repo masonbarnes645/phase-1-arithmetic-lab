a17e4b8b6641f6f14fa1268bc149a87b788c3285
Mason Barnes <barnesmason980@gmail.com>
2024-06-03 16:32:35 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const random = parseInt(Math.random()*100);
+const num1 = 31
+const num2 = 2
+const multiply = (num1*num2)
+const num3 = 64
+const num4 = 6
+const mod = (num3%num4)
+const list = (1,5,20);
+const max = Math.max(list);
 
-// Write your code here
+
